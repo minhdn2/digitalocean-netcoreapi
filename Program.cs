33df3c1b4d2c29b9a098d464cae08cb07bbe5981
Minh Duong <minhduongnhat@outlook.com>
2019-11-10 22:20:57 +0700

    update builder

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace NetCoreApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+            .UseUrls("http://localhost:5002/");
     }
 }
